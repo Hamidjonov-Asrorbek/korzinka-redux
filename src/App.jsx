f25c4a2f279991839f80c76c2afe34f838c25519
Hamidjonov-Asrorbek <asrorbekhamidjonov1@gmail.com>
2024-06-05 07:27:19 +0500

    login-signUp

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,44 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import Layout from "./pages/Layout";
+import Layout from "./layout";
 import "./App.css";
 import Products from "./pages/Products";
+
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import Cards from "./pages/Cards";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Products />}></Route>
-          <Route path="/cart" element={<Cards />}></Route>
-        </Route>
-      </>
-    )
-  );
-  return <RouterProvider router={router} />;
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          index: true,
+          element: <Products />,
+        },
+        {
+          path: "/cart",
+          element: <Cards />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <SignUp />,
+    },
+  ]);
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,13 +1,13 @@
+import Header from "../components/Header";
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../../components/Header";
 
 function Layout() {
   return (
     <>
       <Header />
       <main>
-        <Outlet></Outlet>
+        <Outlet />
       </main>
     </>
   );
